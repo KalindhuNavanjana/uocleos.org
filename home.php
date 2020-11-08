07a60dc23781f5ab63d2153387a75c39d2919fa8
Kalindhu_Navanjana <kalindhunavanjana@gmail.com>
2020-11-08 13:39:15 +0530

    Diagan Alley Live

--- home.php
+++ home.php
@@ -376,12 +376,11 @@
 
 
 
-        <!-- <h2 id="Diagon_Alley" style="margin-bottom:20px;margin-top:40px;text-align:center">Diagon Alley</h2>
+        <h2 id="Diagon_Alley" style="margin-bottom:20px;margin-top:40px;text-align:center">Diagon Alley</h2>
 
         <div class="row-fluid" style="margin-bottom:20px">
-            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fleo.uoc.306c1%2Fvideos%2F256756138883710%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true">
-            </iframe>
-        </div> -->
+        <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fleo.uoc.306c1%2Fvideos%2F296873414712687%2F&width=1280" width="1280" height="720" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+        </div>
 
         <h2 id="Ape_Hadagasma" style="margin-bottom:20px;margin-top:40px;text-align:center">අපේ හදගැස්ම</h2>
 
